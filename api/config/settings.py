6470d85a3165ce04c0cff5e2866dc817c1a28a21
areebh26 <areebulhaq2005@gmail.com>
2026-08-04 22:26:11 +0500

    s

--- api/config/settings.py
+++ api/config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'apps.users',
+    'apps.movie',
     "phonenumber_field",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
